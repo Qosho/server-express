bbbf2ac3cb5cdd10977b684debcdf0a227b68a07
Tzamn Hernandez <tzamnhc@hotmail.com>
2018-10-08 18:29:19 -0500

    First Version of App

---
+++ app-old.js
@@ -0,0 +1,21 @@
+/*
+    App con Web Server
+*/
+
+const server = require('http');
+
+server.createServer((req, res) => {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+
+        let salida = {
+            nombre: 'Tzamn',
+            edad: 25,
+            url: req.url
+        }
+        res.write(JSON.stringify(salida));
+        //res.write('Hola Mundo');
+        res.end();
+    })
+    .listen(8080);
+
+console.log('Escuchando el puerto 8080');
---
+++ hbs/helpers.js
@@ -0,0 +1,15 @@
+const hbs = require('hbs');
+
+//Helpers 
+hbs.registerHelper('getAnio', () => {
+    return new Date().getFullYear()
+});
+
+hbs.registerHelper('capitalizar', (texto) => {
+
+    let palabra = texto.split(' ');
+    palabra.forEach((palabra, idx) => {
+        palabra[idx] = palabra.charAt(0).toUpperCase() + palabra.slice(1).toLowerCase();
+    });
+    return palabra.join(' ');
+});
